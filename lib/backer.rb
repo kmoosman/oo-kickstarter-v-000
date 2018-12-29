34aac0b5721830fe35284259a8836138a0f86191
Katie Moosman <moosman.katie@gmail.com>
2018-12-29 04:38:46 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,14 @@
 class Backer 
   attr_accessor :name
   
+  @backed_projects = []
+  
   def initialize(name)
     @name = name 
   end
+  
+  def back_project(project)
+    @backed_projects << projects
+  end
+  
 end
